7c97934037a9f1c67eb23fcf86abfd305e559a96
usama-imtiaz-07 <osamaimtiaz677@gmail.com>
2025-08-17 01:55:44 +0100

    added audio feature extraction func in main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from models.audio_model import AudioFeatureExtractor
 from models.video_model import VideoFeatureExtractor
+import torch
+import librosa
 
-
+# initialize models
 audio_model = AudioFeatureExtractor()
-video_model = VideoFeatureExtractor()
+#video_model = VideoFeatureExtractor()
+
+# ---- Audio Feature Extraction ----
+
+# load audio
+audio_path = "./data/audio/03-01-01-01-01-01-01.wav"
+waveform, sr = librosa.load(audio_path, sr=16000)
+
+audio_features = audio_model.extract_features(waveform, sr)
+print("Audio feature shape:", audio_features.shape)
+
+# ---- Video Feature Extraction ----
+
